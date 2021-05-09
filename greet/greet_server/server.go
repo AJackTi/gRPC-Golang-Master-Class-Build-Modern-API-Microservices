d1fc97aefeac1c6cdbd54439c971ea5b0ad3709a
AjackTi <dtrong97vn@gmail.com>
2021-05-09 13:26:55 +0700

    gRPC Unary

--- greet/greet_server/server.go
+++ greet/greet_server/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/ajackti/grpc-go-course/greet/greetpb"
 	"google.golang.org/grpc"
@@ -9,6 +10,15 @@ import (
 )
 
 type server struct {
+}
+
+func (*server) Greet(ctx context.Context, req *greetpb.GreetRequest) (*greetpb.GreetResponse, error) {
+	fmt.Printf("Greet function was invoked with %v", req)
+	firstName := req.GetGreeting().GetFirstName()
+	result := "Hello " + firstName
+	res := &greetpb.GreetResponse{Result: result}
+
+	return res, nil
 }
 
 func main() {
